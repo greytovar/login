a9481366dadef9cb19db6ffa1d5388201dc3cb72
Grey Tovar <greytovar90@gmail.com>
2023-04-16 23:32:37 -0400

    Test Login

---
+++ src/main/java/servletPage/LoginServlet.java
@@ -0,0 +1,54 @@
+package servletPage;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String u = request.getParameter("user");
+		String p = request.getParameter("password");
+		PrintWriter out = response.getWriter();
+		response.setContentType("text/html");
+		
+		if (u.equals("test") && p.equals("test")) {
+			response.sendRedirect("success.jsp");
+		} else {
+			out.print("El usuario o clave son incorrectas");
+			RequestDispatcher rd = request.getRequestDispatcher("login.jsp");
+			rd.include(request, response);
+		}
+	}
+
+}
---
+++ src/main/webapp/login.jsp
@@ -0,0 +1,22 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Insert title here</title>
+</head>
+<body>
+<form action="LoginServlet" method="post">
+	<pre>
+		Ingresa Nombre Usuario: <input type="text" name="user"/>
+		
+		         Ingresa Clave: <input type="password" name="password" />
+		
+		             <input type="submit" value="Login" >   <input type="reset" value="Reset" >
+	</pre>
+
+</form>
+
+</body>
+</html>
